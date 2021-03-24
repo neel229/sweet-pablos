77c0afda001af87f76ecf136fde2b726f9b520ea
neel229 <device.neel@gmail.com>
2021-03-24 11:04:58 +0530

    Refactor users db schema

--- internal/user/db/sqlc/models.go
+++ internal/user/db/sqlc/models.go
@@ -3,15 +3,14 @@
 package db
 
 import (
-	"database/sql"
 	"time"
 )
 
 type Uaccount struct {
-	ID        int64          `json:"id"`
-	FirstName string         `json:"first_name"`
-	LastName  sql.NullString `json:"last_name"`
-	Email     string         `json:"email"`
-	Password  string         `json:"password"`
-	Ts        time.Time      `json:"ts"`
+	ID        int64     `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	Ts        time.Time `json:"ts"`
 }
