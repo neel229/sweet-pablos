1609e5b24b292e47200925f52fb55038808b9586
neel229 <device.neel@gmail.com>
2021-03-24 09:38:12 +0530

    Add config.go

---
+++ util/config.go
@@ -0,0 +1,26 @@
+package util
+
+import "github.com/spf13/viper"
+
+// Config contains the env settings used for program
+// configuration
+type Config struct {
+	DBDriver string `mapstructure:"DB_DRIVER"`
+	DBSource string `mapstructure:"DB_SOURCE"`
+	LisAddr  string `mapstructure:"LIS_ADDR"`
+}
+
+// LoadConfig reads a config file given the path
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	if err = viper.ReadInConfig(); err != nil {
+		return
+	}
+	err = viper.Unmarshal(&config)
+	return
+}
